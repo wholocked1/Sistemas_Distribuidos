5d19615880ab57151b5116d644dfec1db6538ce3
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2025-05-16 23:36:45 +0000

    reset do broker e servidor para uma lógica melhor

--- broker.py
+++ broker.py
@@ -1,55 +1,27 @@
 import zmq
-import msgpack
 import time
 
-# precisa verificar a mensagem:
-# pegar todas os pedidos de tempo e passar para o servidor Coordenador
-# pegar a hora do Coordenador e passar para os outros servidores
-# pegar todos os pedidos de eleição e passar para os ranks acima dele
-# pegar as respostas dos outros ranks para que seja enviada a resposta
-# salvar:
-# dicionário com rank (chave) e IP
-# rank do Coordenador
-# tamanho de servidores
-
 #arquivo de log do broker
-lista_ip = {}
+lista_ip = []
 qtde = 3
+rank = 0
 nome = "broker.txt"
 arq = open(nome, "w")
 arq.write("Broker ativado com sucesso (" + time.asctime() + ")\n")
-#leitura do arquivo de IP dos servidores
-ip = open("ip", "r")
-ips = ip.readline()
-print(ips)
-ip.close()
-separado = ips.split(" ")
-#conexão com o servidor por msgpack usando REP
-ctx = zmq.Context() 
-client = ctx.socket(zmq.REP)
-porta = "tcp://*:" + separado[0]
-client.bind(porta)
-lista_ip["1"] = separado[0]
-arq.write(f"Conexão com server 1 na porta {separado[0]} realizada por msgpack (" + time.asctime()+")\n") #escreve no log que a conexão ocorreu
-ctx1 = zmq.Context() 
-client1 = ctx1.socket(zmq.REP)
-porta1 = "tcp://*:" + separado[1]
-client.bind(porta1)
-lista_ip["2"] = separado[1]
-arq.write(f"Conexão com server 2 na porta {separado[1]} realizada por msgpack (" + time.asctime()+")\n") #escreve no log que a conexão ocorreu
-ctx2 = zmq.Context() 
-client2 = ctx2.socket(zmq.REP)
-porta2 = "tcp://*:" + separado[2]
-client.bind(porta2)
-lista_ip["3"] = separado[2]
-arq.write(f"Conexão com server 3 na porta {separado[2]} realizada por msgpack (" + time.asctime()+")\n") #escreve no log que a conexão ocorreu
-#recebe uma mensagem de pedido de hora do servidor
-print(lista_ip)
-msg_p = client.recv()
-msg = msgpack.unpackb(msg_p)
-print(f'Mensagem recebida: {msg}')
-#manda a mensagem para o servidor para colocar a hora certa
-ans = {"hora_atual": time.asctime(), "mensagem":"hora certa"}
-ans_p = msgpack.packb(ans)
-client.send(ans_p)
-print('mensagem enviada')
+
+ctx = zmq.Context()
+pub = ctx.socket(zmq.PUB)
+pub.bind("tcp://*:5559")
+
+ctx = zmq.Context()
+rep = ctx.socket(zmq.REP)
+rep.bind("tcp://*:5558")
+
+while True:
+    messege = rep.recv()
+    arq.write(f"Mensagem recebido do servidor de porta: {messege}, rank enviado: {rank}, horário: {time.asctime()}")
+    lista_ip.append(messege)
+    rep.send(rank)
+    pub.send_string(lista_ip)
+    rank += 1
+
--- servidor3.py
+++ servidor3.py
@@ -1,63 +1,122 @@
 import zmq
 import time
 import msgpack
-
 #servidor recebe o número de rank no terminal ao subir o arquivo
 #servidor deve fazer requisição de hora para o servidor coordenador a hora certa
 #servidor coordenador, se online, deve enviar o horário dele
 #se o servidor coordenador ficar offline, o servidor que fez a requisição verificará os outros servidores para poder realizar a eleição
 #se o servidor for o maior dentre eles, ele será eleito o novo coordenador
 #fazer docker compose
+coordenador = False
+ctx = zmq.Context()
+sub = ctx.socket(zmq.SUB)
+sub.setsockopt_string(zmq.SUBSCRIBE, "")
+sub.connect("tcp://localhost:5558")
+ip = input('IP desse servidor:') #pede o input do número do servidor de forma manual para teste
+ctx = zmq.Context()
+rep = ctx.socket(zmq.REP)
+Ip_Server = "tcp://*:" + ip
+rep.bind(Ip_Server)
 
-qnt = 3 #quantidade de servidores abertos
-coordenador = False #inicia o servidor como não coordenador
-rank = 0 #rank de cada servidor será dado pelo terminal ao iniciar
-rank = int(input("Qual o rank desse servidor:")) #recebe o rank dele
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5559")
+
+socket.send(ip)
+mensagem = socket.recv()
+rank = int(mensagem)
+
+if(rank == 1):
+    coordenador = True
+
 nome = "servidor" + str(rank) + ".txt" #gera o nome do arquivo de log
 arq = open(nome, "w") #abre o arquivo de log
-inicio = time.time #pega o time inical
-arq.write("Servidor aberto (" + time.asctime() + ")\n") #coloca que o servidor foi aberto e o horário inicial
-if(rank == 1):
-    arq_ip = open("ip", "w") #abre um arquivo que coloca os endereços de IP dos servidores
-    ip = input('IP desse servidor:') #pede o input do número do servidor de forma manual para teste
-    arq_ip.write(ip + " ")#escreve no arquivo no número do IP do servidor
-    arq_ip.close()
-else:
-    arq_ip = open("ip", "a") #abre um arquivo que coloca os endereços de IP dos servidores
-    ip = input('IP desse servidor:') #pede o input do número do servidor de forma manual para teste
-    arq_ip.write(ip + " ")#escreve no arquivo no número do IP do servidor
-    arq_ip.close()
-arq.write("O IP desse servidor é: " + ip + " (" + time.asctime() +")\n") #escreve no log qual o IP do servidor
-#abre a conexão do msgpack por Request
-ctx = zmq.Context() 
-client = ctx.socket(zmq.REQ)
-porta = "tcp://localhost:" + ip
-client.connect(porta)
-arq.write("Comunicação via Msgpack aberta (" + time.asctime() + ")\n") #coloca no log que a comunicação foi aberta
-print("ok")
-#envia mensagem para o broker para pedir a hora certa
-msg = {"mensagem": "quero hora", "timestamp": time.asctime()}
-msg_p = msgpack.packb(msg)
-client.send(msg_p)
-print('mensagem enviada')
-#recebe a mensagem de hora certa e mostra no console
-msg_p = client.recv()
-msg = msgpack.unpackb(msg_p)
-print(f'Mensagem recebida: {msg}')
+inicio = time.time() #pega o time inical
+arq.write(f"Servidor aberto ({time.ctime(inicio)})\n") #coloca que o servidor foi aberto e o horário inicial
 
+while True:
+    #tem que fazer o poller para dectar mensagens do PUB
+
+
+# qnt = 3 #quantidade de servidores abertos
+# coordenador = False #inicia o servidor como não coordenador
+# rank = 0 #rank de cada servidor será dado pelo terminal ao iniciar
+# rank = int(input("Qual o rank desse servidor:")) #recebe o rank dele
+# nome = "servidor" + str(rank) + ".txt" #gera o nome do arquivo de log
+# arq = open(nome, "w") #abre o arquivo de log
+# inicio = time.time() #pega o time inical
+# arq.write(f"Servidor aberto ({time.ctime(inicio)})\n") #coloca que o servidor foi aberto e o horário inicial
+# if(rank == 1):
+#     arq_ip = open("ip", "w") #abre um arquivo que coloca os endereços de IP dos servidores
+#     ip = input('IP desse servidor:') #pede o input do número do servidor de forma manual para teste
+#     arq_ip.write(ip + " ")#escreve no arquivo no número do IP do servidor
+#     arq_ip.close()
+# else:
+#     arq_ip = open("ip", "a") #abre um arquivo que coloca os endereços de IP dos servidores
+#     ip = input('IP desse servidor:') #pede o input do número do servidor de forma manual para teste
+#     arq_ip.write(ip + " ")#escreve no arquivo no número do IP do servidor
+#     arq_ip.close()
+# arq.write(f"O IP desse servidor é: " + ip + f"({ time.ctime(inicio)})\n") #escreve no log qual o IP do servidor
+# #abre a conexão do msgpack por Request
+# ctx = zmq.Context() 
+# client = ctx.socket(zmq.REQ)
+# porta = "tcp://localhost:" + ip
+# client.connect(porta)
+# arq.write("Comunicação via Msgpack aberta (" + f"{time.ctime(inicio)}" + ")\n") #coloca no log que a comunicação foi aberta
+# print("ok")
+# #envia mensagem para o broker para pedir a hora certa
+# # msg = {"mensagem": "quero hora", "timestamp": time.localtime(inicio)}
+# # msg_p = msgpack.packb(msg)
+# # client.send(msg_p)
+# # print('mensagem enviada')
+# # #recebe a mensagem de hora certa e mostra no console
+# # msg_p = client.recv()
+# # msg = msgpack.unpackb(msg_p)
+# # print(f'Mensagem recebida: {msg}')
+# if(rank == 1):
+#     coordenador = True
+#     time.sleep(15)
 # while True:
 #     if(coordenador == False):
 #         #sub da hora e pedir por eleições
+#         msg = {"mensagem": "quero hora", "timestamp": time.ctime(inicio)}
+#         msg_p = msgpack.packb(msg)
+#         client.send(msg_p)
+#         print('mensagem enviada')
+#         arq.write("Pedido de hora realizado (" + f"{time.ctime(inicio)}" + ")")
+#         msg_p = client.recv()
+#         msg = msgpack.unpackb(msg_p)
+#         print(f'Mensagem recebida: {msg}')
+        
+#         if(msg["mensagem"] == 1):
+#             msg = {"mensagem":"eleicao", "timestamp": time.ctime(inicio)}
+#             msg_p = msgpack.packb(msg)
+#             client.send(msg_p)
+#         else:
+#             inicio = inicio + msg_p["timestamp"]
+#             arq.write(f"Hora atualizada com sucesso ({time.ctime(inicio)})")
 #         #eleição
-#         for i in range(rank+1,qnt+1):
-#             tempo = time.time
-#             msg = {"mensagem": "eleicao", "timestamp": tempo, "servidor": i}
-#             arq.write("Requisição de eleição feita em: " + tempo + "para o servidor: " + i + "\n")
-#             #envia mensagem para broker
-#         #ou
-#         tempo = time.time
-#         msg = {"mensagem": "eleicao", "timestamp": tempo, "servidor": rank}
-#         arq.write("Requisição de eleição feita em: " + tempo)
+#         # for i in range(rank+1,qnt+1):
+#         #     tempo = time.time
+#         #     msg = {"mensagem": "eleicao", "timestamp": tempo, "servidor": i}
+#         #     arq.write("Requisição de eleição feita em: " + tempo + "para o servidor: " + i + "\n")
+#         #     #envia mensagem para broker
+#         # #ou
+#         # tempo = time.time
+#         # msg = {"mensagem": "eleicao", "timestamp": tempo, "servidor": rank}
+#         # arq.write("Requisição de eleição feita em: " + tempo)
 #         #recebe a mensagem
+        
 #     else:
 #         #pub da hora
+#         msg_p = client.recv()
+#         msg = msgpack.unpackb(msg_p)
+#         print(f'Mensagem recebida: {msg}')
+#         arq.write("Pedido de hora recebido (" + time.ctime() + ")")
+#         msg = {"mensagem": "hora certa", "timestamp": time.time()}
+#         msg_p = msgpack.packb(msg)
+#         client.send(msg_p)
+#         print('mensagem enviada')
+#         arq.write(f"Hora enviada com sucesso ({time.ctime()})")
+#     inicio = inicio + rank
+
